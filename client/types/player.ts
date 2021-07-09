e68fc39e267ce4f18167869d1cdad3e6ea74d04c
Ihar Herasiuk <Ihar.Hersiuk@itechart-group.com>
2021-07-09 18:10:41 +0300

    same

--- client/types/player.ts
+++ client/types/player.ts
@@ -7,3 +7,36 @@ export interface PlayerState{
     currentTime: number,
     pause:boolean,
 }
+
+export enum PlayerActionTypes {
+    PLAY = "PLAY",
+    PAUSE = "PAUSE",
+    SET_DURATION = "SET_DURATION",
+    SET_VOLUME = "SET_VOLUME",
+    SET_CURRENT_TIME = "SET_CURRENT_TIME",
+    SET_ACTIVE = "SET_ACTIVE",
+}
+
+interface PlayAction {
+    type: PlayerActionTypes.PLAY
+}
+interface PauseAction {
+    type: PlayerActionTypes.PAUSE
+}
+interface SetVolumeAction {
+    type: PlayerActionTypes.SET_VOLUME,
+    payload: number;
+}
+interface SetDurationAction {
+    type: PlayerActionTypes.SET_DURATION,
+    payload:number;
+}
+interface SetCurrentTimeAction {
+    type: PlayerActionTypes.SET_CURRENT_TIME,
+    payload:number;
+}
+interface SetActiveAction {
+    type: PlayerActionTypes.SET_ACTIVE,
+    payload: ITrack
+}
+ export type PlayerAction = PauseAction | PlayAction | SetVolumeAction | SetDurationAction | SetCurrentTimeAction | SetActiveAction;
