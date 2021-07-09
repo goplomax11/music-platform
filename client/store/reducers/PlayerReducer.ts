818384e3c8d330f9894badc1dc13ad65d55fcd7d
Ihar Herasiuk <Ihar.Hersiuk@itechart-group.com>
2021-07-09 18:10:32 +0300

    add reducer and types

--- client/store/reducers/PlayerReducer.ts
+++ client/store/reducers/PlayerReducer.ts
@@ -1,12 +1,32 @@
-const initialState = {
+import { PlayerAction, PlayerState } from "../../types/player"
 
+const initialState : PlayerState = {
+    active: null,
+    volume: 50,
+    duration: 0,
+    currentTime:0,
+    pause:true
 }
 
-export const PlayerReducer = ( state = initialState, action ) =>{
+export const PlayerReducer = ( state = initialState, action:PlayerAction ) : PlayerState =>{
     switch (action.type) {
-        // case value:
-            
-        //     break;
+         case "PLAY":
+            return {...state, pause: false}
+    
+         case "PAUSE":
+            return {...state, pause: true}
+    
+         case "SET_DURATION":
+            return {...state, duration : action.payload }
+    
+         case "SET_VOLUME":
+            return {...state, volume: action.payload}
+    
+         case "SET_CURRENT_TIME":
+            return {...state, currentTime:action.payload}
+
+        case "SET_ACTIVE":
+            return {...state, active:action.payload}
     
         default:
             return state
